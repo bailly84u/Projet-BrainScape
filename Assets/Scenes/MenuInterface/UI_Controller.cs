0b012447925c2db2b0f705001cc22a75118f4344
bailly84u <sebastien.bailly1@etu.univ-lorraine.fr>
2020-02-12 13:04:23 -0500

    interface user

---
+++ Assets/Scenes/MenuInterface/UI_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Controller : MonoBehaviour
+{
+
+    public Text buttonText;
+    // Start is called before the first frame update
+    public void Click()
+    {
+        buttonText.text = "C est fait";
+    }
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+
+}
