dc0173644c27135c6258c6c3e09e3025ba9b71c2
bailly84u <sebastien.bailly1@etu.univ-lorraine.fr>
2020-02-12 20:14:00 -0500

    Ajout Menu in-game + changement de scene

---
+++ Assets/Scenes/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void goLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void returnMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
