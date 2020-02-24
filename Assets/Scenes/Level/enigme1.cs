d9294afb02d66c8188f5fa90c4054a5c913c408b
bailly84u <sebastien.bailly1@etu.univ-lorraine.fr>
2020-02-24 13:48:49 -0500

    Avancement archipel

--- Assets/Scenes/Level/enigme1.cs
+++ Assets/Scenes/Level/enigme1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class enigme1 : MonoBehaviour
 {
-
+	public GameObject lien1;
     public GameObject ile1;
     public GameObject ile2;
     public GameObject ile3;
@@ -13,10 +13,15 @@ public class enigme1 : MonoBehaviour
     public GameObject ile6;
     public GameObject ile7;
     public GameObject reset;
+	
+
+	public bool[] etat_iles;
+	public int etat_clic = 0;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+         etat_iles = new bool[] { false,false,false,false,false,false,false,false,false,false};
     }
 
     // Update is called once per frame
@@ -26,10 +31,33 @@ public class enigme1 : MonoBehaviour
     }
 
     //Retourne Vrai si les attaches sont correctement mises, activer via un bouton ? 
-    bool verifier_etat()
+	public bool verifier_etat()
     {
         return true;
         //Si l'ile 1 a une attache (chercher le script) avec les bonnes iles et que elles, puis si ile 2 ....
         //Retourne Vrai
     }
+	
+	public void cliquerBoutonNonActif(int numero)
+	{
+		
+		if (etat_clic<2)
+		{
+			
+		   etat_iles[numero] = !etat_iles[numero];
+		   
+		   etat_clic +=1;
+		}
+		else
+		{
+			fairelien();
+			etat_clic=0;
+		}
+
+    }	
+	
+	private void fairelien()
+	{
+		lien1.SetActive(true);
+    }	
 }
