97835270314967d165bed20541035779deab91b8
bailly84u <sebastien.bailly1@etu.univ-lorraine.fr>
2020-03-12 11:10:07 -0400

    ajout code seb

---
+++ Assets/Scenes/Level/ParticuleDetector.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticuleDetector : MonoBehaviour
+{
+    [HideInInspector]
+    public int compteurSolide1;
+    public int compteurSolide2;
+    public int compteurSolide3;
+    public int compteurSolide4;
+    public GameObject ModeleSolidePlein;
+
+    private void Start()
+    {
+        compteurSolide1 = 0;
+        compteurSolide2 = 0;
+    }
+    private void Update()
+    {
+        if (compteurSolide1==4 && compteurSolide2==2)
+        {
+            ModeleSolidePlein.SetActive(true);
+
+        }
+      
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        print("test");
+        if (col.gameObject.tag.Equals("Solide1"))
+        {
+            compteurSolide1++;
+
+        }
+        if (col.gameObject.tag.Equals("Solide2"))
+        {
+            compteurSolide2++;
+
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        print("test2");
+        if (col.gameObject.tag.Equals("Solide1"))
+        {
+            compteurSolide1--;
+
+        }
+        if (col.gameObject.tag.Equals("Solide2"))
+        {
+            compteurSolide2--;
+
+        }
+    }
+}
